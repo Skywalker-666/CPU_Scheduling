28710eb36d0b2949733d4e6b9275adf35bfe32fa
Shreyas_Kumar_Patel <169144956+Skywalker-666@users.noreply.github.com>
2024-06-20 10:50:36 +0530

    Create SJF.cpp

---
+++ SJF.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter the number of processes"<<endl;
+	cin>>n;
+	vector<int> v1(n),v2(n);
+	cout<<"Enter the burst time:"<<endl;
+	for(int i=0;i<n;i++){
+		cin>>v1[i];
+	}
+	sort(v1.begin(),v1.end());
+	int wt=0;
+	v2[0]=wt;
+	for(int i=1;i<n;i++){
+		wt+=v1[i-1];
+		v2[i]=wt;
+	}
+	int sum=0;
+	for(int i=0;i<n;i++){
+		sum+=v2[i];
+	}
+	float av=sum/n;
+	cout<<"average waiting time is"<<av<<endl;
+}
